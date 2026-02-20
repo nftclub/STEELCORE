4369d383851d0fad8047c91b41d51c797d0687b6
nftclub <82267714+nftclub@users.noreply.github.com>
2026-02-20 21:08:23 +0300

    add privacy policy

---
+++ app/src/main/java/com/steelcore/app/DelegationService.java
@@ -0,0 +1,14 @@
+package com.steelcore.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
